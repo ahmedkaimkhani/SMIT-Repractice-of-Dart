a4e10a73ceac2a6bac6231e86cc4229e0bd1aa28
ahmedkaimkhani <ak9302359@gmail.com>
2025-07-16 03:25:46 +0500

    Repractice of Dart SMIT

---
+++ loops_practice.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  var email = (stdin.readLineSync()!);
+  var password = stdin.readLineSync()!;
+  bool isLogin = false;
+
+  while (isLogin == false) {
+    if (email == "ahmed@gmail.com" && password == "123456") {
+      print("Login Successful");
+      isLogin = true;
+    } else {
+      print("Login Failed");
+    }
+  }
+}
