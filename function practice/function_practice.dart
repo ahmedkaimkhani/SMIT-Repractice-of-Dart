2269b23652a063f8e352a307b077c38e0bd0aa96
ahmedkaimkhani <ak9302359@gmail.com>
2025-07-18 04:05:20 +0500

    Repractice of Dart Functions SMIT

--- function practice/function_practice.dart
+++ function practice/function_practice.dart
@@ -1,19 +1,32 @@
 void main() {
-  var result = studentMarkSheet();
-  print(result);
+  print(sumOfTwoNumbers(5, 2));
+  print(sumOfTwoNumbers(3, 5));
+  print(sumOfTwoNumbers(10, 13));
 }
 
-studentMarkSheet() {
-  String name = "Ahmed";
-  num english = 79;
-  num physics = 75;
-  num math = 85;
-  num obtainMarks = english + physics + math;
-  num percentage = (obtainMarks / 300) * 100;
-
-  if (percentage > 50) {
-    return "$name pass hogya hai";
-  } else {
-    return "$name fail hogya hai";
-  }
+num sumOfTwoNumbers(int num1, int num2) {
+  num result = num1 + num2;
+  return result;
 }
+
+// Functions Repractice 
+
+// void main() {
+//   var result = studentMarkSheet();
+//   print(result);
+// } 
+
+// studentMarkSheet() {
+//   String name = "Ahmed";
+//   num english = 79;
+//   num physics = 75;
+//   num math = 85;
+//   num obtainMarks = english + physics + math;
+//   num percentage = (obtainMarks / 300) * 100;
+
+//   if (percentage > 50) {
+//     return "$name pass hogya hai";
+//   } else {
+//     return "$name fail hogya hai";
+//   }
+// }
