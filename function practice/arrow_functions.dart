084e22145e0d960aa12a9cb6e5e960ee39a1df12
ahmedkaimkhani <ak9302359@gmail.com>
2025-07-22 02:40:33 +0500

    Repractice of functions dart SMIT

--- function practice/arrow_functions.dart
+++ function practice/arrow_functions.dart
@@ -2,6 +2,26 @@ void main() {
   print(sumOfTwoNumbers(5, 7));
 }
 
-sumOfTwoNumbers(int num1, int num2) {
-  return num1 + num2;
-}
+sumOfTwoNumbers(int num1, int num2) => num1 + num2;
+
+
+// Arrow Functions
+// Arrow functions hum tab use krty hain jab hamari login 1 line
+// kii ho or readability kay liye ek line se bari login hum simpe function me krty hain.
+
+// void main() {
+//   print(sumOfTwoNumbers(5, 7));
+// }
+
+// sumOfTwoNumbers(int num1, int num2) => num1 + num2;
+
+
+// Simple Functions 
+
+// void main() {
+//   print(sumOfTwoNumbers(5, 7));
+// }
+
+// sumOfTwoNumbers(int num1, int num2) {
+//   return num1 + num2;
+// }
