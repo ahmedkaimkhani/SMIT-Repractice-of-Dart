eac3c2d8eedae2d637c3a71d037de36e73d66979
ahmedkaimkhani <ak9302359@gmail.com>
2025-07-23 04:16:46 +0500

    REpractice of ternary Operators Dart SMIT

---
+++ null safety and ternary operators/ternary_operators.dart
@@ -0,0 +1,15 @@
+void main() {
+  int age = 18;
+  String faceBook = age >= 18 ? "Allow" : "Not Allow";
+  print(faceBook);
+}
+
+// yeh bilkul if else ki tarha he work krty hain, structure change hai
+// for exp ager koi person 18 ya 18+ hai tw usy fb use allow krdo otherwise Not Allow.
+
+// void main() {
+//   int age = 18;
+//   String faceBook = age >= 18 ? "Allow" : "Not Allow";
+//   print(faceBook);
+// }
+
