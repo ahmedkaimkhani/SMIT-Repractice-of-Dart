0aea9e9f7e5e71ef901a71c41bb478818ec190a4
ahmedkaimkhani <ak9302359@gmail.com>
2025-07-24 04:38:57 +0500

    Repractice of Loops Dart SMIT

--- loops practice/loops_starick.dart
+++ loops practice/loops_starick.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 void main() {
   for (var i = 1; i < 5; i++) {
     for (var j = 1; j <= i; j++) {
-      stdout.write(i);
+      stdout.write("*");
     }
-    print('');
+    print("");
   }
 }
 
@@ -19,3 +19,8 @@ void main() {
 //     print('');
 //   }
 // }
+// Output: 
+// 1
+// 22
+// 333
+// 4444
