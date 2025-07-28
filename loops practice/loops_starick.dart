75d641b780a97b05a1f3d7f7432740ddb03af608
ahmedkaimkhani <ak9302359@gmail.com>
2025-07-29 04:07:54 +0500

    Repractice of Loops Dart SMIT

--- loops practice/loops_starick.dart
+++ loops practice/loops_starick.dart
@@ -3,10 +3,11 @@ import 'dart:io';
 void main() {
   var rows = 5;
   var space = " ";
+  var counter = 1;
   for (var i = 1; i < rows; i++) {
     stdout.write("${space * (rows - i)}");
     for (var j = 1; j <= i; j++) {
-      stdout.write("* ");
+      stdout.write("${counter++} ");
     }
     print("");
   }
@@ -27,3 +28,42 @@ void main() {
 // **
 // ***
 // ****
+
+// Example 2
+
+// void main() {
+//   var rows = 5;
+//   var space = " ";
+//   for (var i = 1; i < rows; i++) {
+//     stdout.write("${space * (rows - i)}");
+//     for (var j = 1; j <= i; j++) {
+//       stdout.write("* ");
+//     }
+//     print("");
+//   }
+// }
+// Output: 
+//     * 
+//    * * 
+//   * * * 
+//  * * * *
+
+// Example 3
+
+// void main() {
+//   var rows = 5;
+//   var space = " ";
+//   var counter = 1;
+//   for (var i = 1; i < rows; i++) {
+//     stdout.write("${space * (rows - i)}");
+//     for (var j = 1; j <= i; j++) {
+//       stdout.write("${counter++} ");
+//     }
+//     print("");
+//   }
+// }
+// Output:
+//     1 
+//    2 3 
+//   4 5 6 
+//  7 8 9 10 
